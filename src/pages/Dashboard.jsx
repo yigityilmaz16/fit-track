d15cafafc829d9db21f1e198612e46e4d1f111cc
yigityilmaz16 <yigityilmaz196345@gmail.com>
2026-07-18 11:59:30 +0300

    feat: add dashboard analytics and workout summaries

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,25 +1,44 @@
 import "./Dashboard.css";
+import { useContext } from "react";
+import { WorkoutContext } from "../context/WorkoutContext";
 
 function Dashboard() {
+  const {workouts} = useContext(WorkoutContext);
+  const totalSets= workouts.reduce((acc,workout)=>{
+    return acc+ workout.sets
+  },0)
+  const totalVolume= workouts.reduce((acc,workout)=>{
+    return acc+(workout.weight * workout.reps * workout.sets )
+  },0)
+  const lastWorkout =
+  workouts.length > 0
+    ? workouts.reduce((acc, workout) => {
+        return acc.date > workout.date ? acc : workout;
+      }, workouts[0])
+    : null;
   return (
     <>
           <h1>FitTrack Dashboard</h1>
     <div className="dashboard">
       <div className="stat-card">
-        <h3>Toplam Antrenman</h3>
-        <p>0</p>
+        <h3>Total Workout</h3>
+        <p>{workouts.length}</p>
       </div>
       <div className="stat-card">
-        <h3>Toplam Set</h3>
-        <p>0</p>
+        <h3>Total Sets</h3>
+        <p>{totalSets}</p>
       </div>
       <div className="stat-card">
-        <h3>Bu Hafta</h3>
-        <p>0</p>
+        <h3>Total Volume</h3>
+        <p>{totalVolume}</p>
       </div>
       <div className="stat-card">
         <h3>Son Antrenman</h3>
-        <p>-</p>
+        <p>
+            {lastWorkout
+         ? `${lastWorkout.exercise} , ${lastWorkout.date}`
+         : "Henüz antrenman yok"}
+       </p>
       </div>
     </div>
     </>
